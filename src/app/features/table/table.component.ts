2046643e273cdcac50b441edc02b2595645702e0
andrejuafor <andrejuafor1999@gmail.com>
2024-09-06 22:57:50 -0600

    feat: The header and the table section were finished layout

--- src/app/features/table/table.component.ts
+++ src/app/features/table/table.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, SharedModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
-
+  public faCircleCheck = faCircleCheck;
+  list = [
+    {
+      title: 'Instance Coupons',
+      no_card_status: true,
+      link_card_status: true,
+    },
+    {
+      title: 'Full access to Visa Saving Edge benefits',
+      no_card_status: false,
+      link_card_status: true,
+    },
+    {
+      title: 'Cashback tracking',
+      no_card_status: false,
+      link_card_status: true,
+    },
+    {
+      title: 'Merchant location search',
+      no_card_status: false,
+      link_card_status: true,
+    },
+    {
+      title: 'cashback offers',
+      no_card_status: false,
+      link_card_status: true,
+    },
+  ]
 }
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -2,21 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-button',
   template: `
-    <button mat-stroked-button [ngClass]="type">
+    <button [ngClass]="type">
       {{ title }}
+       <fa-icon [icon]="faChevronRight"></fa-icon>
     </button>
   `,
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   @Input() title!: string;
   @Input() icon!: string;
   @Input() type!: string;
-  //Flat
-  //Stroked
+
+  public faChevronRight = faChevronRight
 }
