d9fd1561a58c49fa45037f227526c6faedd629d2
andrejuafor <andrejuafor1999@gmail.com>
2024-09-06 18:50:32 -0600

    feat: the following components have been created: brand, detail, footer, header, menu, table within featuer and a main-layout was added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/es",
+    pathMatch: "full"
+  },
+  {
+    path: ":lang",
+    loadChildren: () =>
+      import("./layout/main-layout/main-layout.module").then((m) => m.MainLayoutModule),
+  },
+  // {
+  //   path: ":lang/not-found/error",
+  //   loadChildren: () =>
+  //     import("./modules/errors/errors.module").then(
+  //       (m) => m.ErrorsModule
+  //     ),
+  // },
+  // {
+  //   path: ":lang/:merchant",
+  //   canActivate: [AuthGuard],
+  //   loadChildren: () =>
+  //     import("./pages/layout.module").then((m) => m.LayoutModule),
+  // },
+];
