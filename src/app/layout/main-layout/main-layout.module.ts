02ed656728881e0dae785f366c2038be585e231b
andrejuafor <andrejuafor1999@gmail.com>
2024-09-06 19:41:09 -0600

    feat: button added as a component

--- src/app/layout/main-layout/main-layout.module.ts
+++ src/app/layout/main-layout/main-layout.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
+import { HeaderComponent } from '../../features/header/header.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MainLayoutRoutingModule
+    MainLayoutRoutingModule,
   ]
 })
 export class MainLayoutModule { }
