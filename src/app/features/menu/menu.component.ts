9dc317a00f4bbe42e7edc9ddcad31881c3b15b7d
andrejuafor <andrejuafor1999@gmail.com>
2024-09-07 00:31:10 -0600

    feat: tags section added

--- src/app/features/menu/menu.component.ts
+++ src/app/features/menu/menu.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  public currentCategory!: string;
+  readonly categories = signal([
+    { name: 'Featured' },
+    { name: 'Restaurants' },
+    { name: 'Travel' },
+    { name: 'Fuel' },
+    { name: 'Service' },
+    { name: 'Gifts & Entretaiment' },
+    { name: 'Shopping' },
+    { name: 'Electronics' },
+    { name: 'Software' },
+    { name: 'Health & Beauty' },
+    { name: 'Office Supceas' },
+    { name: 'Autoservice' },
+  ]);
 
+  public filterByCategory(category: string): void {
+    console.log(category);
+    this.currentCategory = category;
+  }
 }
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../features/header/header.component';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { SharedModule } from '../../shared/shared.module';
 import { TableComponent } from '../../features/table/table.component';
+import { MenuComponent } from '../../features/menu/menu.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [TableComponent],
+  imports: [TableComponent, MenuComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
